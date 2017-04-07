0ce33c33103878a6ac03ab0e03492ea2392629c8
dumebi <dikejude49@gmail.com>
2017-04-07 11:12:36 +0100

    made small changes to app.js and chat/index.js for uploading files to he chat

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    cookie: {maxAge: 180 * 60 * 1000}
+    cookie: {maxAge: 1800 * 600 * 10000000}
 }));
 app.use(flash());
 app.use(passport.initialize());
