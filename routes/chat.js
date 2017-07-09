e44f3d5224a472bb8f577d865d7a1607da4f42ce
tay.andrew <tay.andrew@yahoo.com>
2017-07-09 23:43:46 +0100

    chat fix

--- routes/chat.js
+++ routes/chat.js
@@ -1,53 +1,28 @@
-var express = require('express');
-var router = express.Router();
-var csrf = require('csurf');
-var Room = require('../models/room');
+let express = require('express');
+let router = express.Router();
+let csrf = require('csurf');
+let Room = require('../models/room');
 let _ = require('underscore')._;
 let multer = require('multer');
 const fs = require('fs');
-var utils = require('../utils/api');
+let utils = require('../utils/api');
 
 let csrfProtection = csrf();
 
-router.post('/chathistory', function(req, res, next) {
+router.post('/chathistory', (req, res, next) => {
     let room = req.body.room;
     let chatHistory = req.body.chatHistory;
     console.log("chat history");
     console.log(chatHistory);
-    Room.update({room_id: room.id}, {history: chatHistory}, function (err, result) {
-        if (err) {
+
+    Room.update({room_id: room.id}, {history: chatHistory})
+        .then(room => {
+            return res.send({message: 'success', result: room});
+        })
+        .catch(err => {
             console.log(err);
-            return res.status(500);
-        }
-        console.log(result);
-        //return res.status(200);
-    });
-   // return res.status(200);
-});
-
-router.post('/room-chat', utils.isActivated, function(req, res, next) {
-    var room_id = req.body.room_id;
-    var chatHistory = req.body.chat_history;
-
-    Room.findOne({'room_id':room_id}, function (err, room) {
-        if(err){
-            return done(room);
-        }
-        if(!room){
-            req.flash('error', 'User does not exist');
-            return res.redirect('/user/activate');
-        }
-        //
-        // user.is_activated = 1;
-        // user.save(function(err) {
-        //     if (err)
-        //         console.log('error');
-        //     else
-        //         console.log('success');
-        // });
-        // return res.redirect('/');
-    });
-    return res.status(200);
+            return res.send({message: 'failed'});
+        });
 });
 
 router.use(csrfProtection);
@@ -67,28 +42,23 @@ let upload = multer({
 });
 
 /* GET home page. */
-router.get('/', utils.isActivated, function(req, res, next) {
+router.get('/', utils.isActivated, (req, res, next) => {
     //console.log(req.user);
     res.render('chat/index', {layout: false, utils:utils, user: req.user, csrfToken: req.csrfToken()});
 });
 
-router.post('/password', function(req, res, next) {
-    //console.log(req.user);
+router.post('/password', (req, res, next) => {
     let password = req.body.password;
-
-    //console.log("password "+password);
     let pass = utils.getLong(password);
-
-    //console.log(""+pass);
 
     return res.json({password:pass});
 });
 
-router.post('/upload', utils.isActivated, upload.single('attachment'), function(req, res, next){
+router.post('/upload', utils.isActivated, upload.single('attachment'), (req, res, next) =>{
     return res.status(200).send(req.file);
 });
 
-router.post('/uploads/delete', utils.isActivated, function(req, res, next){
+router.post('/uploads/delete', utils.isActivated, (req, res, next) =>{
     let file = req.body.file;
 
     fs.exists('./public/uploads/room/'+file.originalname, (exists) => {
@@ -106,94 +76,66 @@ router.post('/uploads/delete', utils.isActivated, function(req, res, next){
     return res.status(200);
 });
 
-router.post('/add-room', utils.isActivated, function(req, res, next) {
-    var room = req.body;
+router.post('/add-room', utils.isActivated, (r, s, n) => {
+    let room = r.body;
     console.log("room sent: ");
     console.log(room);
 
+    let password = room.password == '' ? null : utils.setLong(room.password);
 
-    if(room.password == ''){
-        var newRoom =  new Room({
-            name: room.name,
-            room_id: room.id,
-            creator: room.creator,
-            members: room.members,
-            isDM: room.isDM,
-            isPrivate: room.isPrivate
+    let newRoom =  new Room({
+        name: room.name,
+        room_id: room.id,
+        creator: room.creator,
+        members: room.members,
+        isDM: room.isDM,
+        isPrivate: room.isPrivate,
+        password: password
+    });
+    newRoom.save()
+        .then(room => {
+            console.log("room saved: "+room.room_id);
+            console.log("room password saved: "+room.room_id);
+            return s.send({message: 'success', result: room });
+        })
+        .catch(err => {
+            return s.send({message: 'failed', result: 'Room could not be saved'});
         });
-        newRoom.save(function (err, result) {
-            if(err){
-                console.log(err);
-            }
-
-            console.log("room saved: "+newRoom.room_id);
-            console.log("room password saved: "+newRoom.room_id);
-            res.json(200);
-        });
-
-        //return res.send(200);
-    }
-    else {
-        var newRoom2 =  new Room({
-            name: room.name,
-            room_id: room.id,
-            creator: room.creator,
-            members: room.members,
-            isDM: room.isDM,
-            isPrivate: room.isPrivate,
-            password: utils.setLong(room.password)
-        });
-        newRoom2.save(function (err, result) {
-            if(err){
-                console.log(err);
-            }
-            console.log("room password saved: "+newRoom2);
-            return res.json(200);
-        });
-
-        //return res.send(200);
-    }
 
 
 });
 
-router.post('/update-room', utils.isActivated, function(req, res, next) {
+router.post('/update-room', utils.isActivated, (r, s, n)  => {
+    let room = r.body;
 
-    var newroom = req.body;
-    console.log("update room: "+newroom.id);
-    console.log(newroom.members);
-
-    if(newroom.creator != req.user.id){
-        console.log("not equal");
-        var members = newroom.members;
-        //console.log(members);
-        var isArray = _.isArray(members);
-        console.log(isArray);
+    if(room.creator != r.user.id){
+        let members = newroom.members;
+        let isArray = _.isArray(members);
         if(isArray) {
-            Room.update({room_id: newroom.id}, {$addToSet: {members: {$each: members}}}, function (err, result) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log(result);
-                return res.json(200);
-            });
+            Room.update({room_id: newroom.id}, {$addToSet: {members: {$each: members}}})
+                .then(room => {
+                    return s.send({message: 'success', result: room });
+                })
+                .catch(err => {
+                    return s.send({message: 'failed', result: 'Room could not be updated'});
+                });
         }
     }
 
-    //return res.send(200);
 });
 
 
 router.get('/room-members/:id', utils.isActivated, function(req, res, next) {
-    var room_id = req.params.id;
+    let room_id = req.params.id;
 
     Room.find({'room_id': room_id }).populate("members").exec(function(err,results){
         if(err){
             console.log(err);
+            return res.send({});
         }
         let room = results[0];
         let members = room.members;
-        console.log(members);
+        //console.log(members);
         res.json(members);
     });
 
@@ -236,7 +178,7 @@ router.get('/room-creator/:id', utils.isActivated, function(req, res, next) {
 
 router.post('/room-creator', utils.isActivated, function(req, res, next) {
 
-    var rooms_id = req.body.rooms;
+    let rooms_id = req.body.rooms;
 
     console.log("rooms id");
     console.log(rooms_id);
